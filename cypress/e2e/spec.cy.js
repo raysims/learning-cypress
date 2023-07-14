7fb857d8b84b009e7d1c80bbd01c7bbe9d2ad31f
raysims <rsims515@gmail.com>
2023-07-14 09:50:25 -0400

    Adding initial tests and cypress.json

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,3 +3,19 @@ describe('template spec', () => {
     cy.visit('https://example.cypress.io')
   })
 })
+
+describe('My first test with cypress', () => {
+    it('True should be True', () => {
+        expect(true).to.equal(true);
+    })
+
+    it('False should be false', () => {
+        expect(false).to.equal(false);
+    })
+})
+
+describe('Simple test that fails', () => {
+    it('Test Should Fail', () => {
+        expect(true).to.equal(false);
+    })
+})
